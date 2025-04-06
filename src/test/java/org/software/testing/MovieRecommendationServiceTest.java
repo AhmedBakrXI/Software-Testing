0d1835ec867af94b6d8cc3b23994252cca590e69
waelmarwan7 <91396631+waelmarwan7@users.noreply.github.com>
2025-04-06 17:05:09 +0200

    Update MovieRecommendationServiceTest.java

--- src/test/java/org/software/testing/MovieRecommendationServiceTest.java
+++ src/test/java/org/software/testing/MovieRecommendationServiceTest.java
@@ -5,5 +5,20 @@ import org.junit.Test;
 public class MovieRecommendationServiceTest {
     @Test
     public void generateRecommendations() {
+        List<User> users = Arrays.asList(new User("Alice"), new User("Bob"));
+        List<Movie> movies = Arrays.asList(new Movie("Inception"), new Movie("The Matrix"));
+
+        MovieRecommendationService movieService = new MovieRecommendationService(new RecommendationStrategy());
+
+        Map<User, List<Movie>> recommendationList = movieService.generateRecommendations(users, movies);
+
+        // Basic assertions to check if all users got all movies recommended
+        assertEquals(2, recommendationList.size());
+        for (User user : users) {
+            assertTrue(recommendationList.containsKey(user));
+            List<Movie> recommended = recommendationList.get(user);
+            assertEquals(2, recommended.size());
+            assertTrue(recommended.containsAll(movies));
+        }
     }
 }
