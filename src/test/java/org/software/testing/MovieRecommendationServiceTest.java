12a7460a1c6fa23921125104470ed44023005784
AhmedBakrXI <ahmedbakr2501@gmail.com>
2025-04-07 14:00:14 +0200

    fix: add import statements

--- src/test/java/org/software/testing/MovieRecommendationServiceTest.java
+++ src/test/java/org/software/testing/MovieRecommendationServiceTest.java
@@ -2,18 +2,26 @@ package org.software.testing;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class MovieRecommendationServiceTest {
     @Test
-    public void generateRecommendations() {
+    public void generateRecommendations() throws IOException {
         String path1 = "test_files/users1.txt";
         String path2= "test_files/movies1.txt";
 
         
+
         List<User> users = FileReader.readUsers(path1);
         List<Movie> movies = FileReader.readMovies(path2);
 
         
-        MovieRecommendationService movieService = new MovieRecommendationService(new RecommendationStrategy());
+        MovieRecommendationService movieService = new MovieRecommendationService(new GenreBasedRecommendation());
 
         Map<User, List<Movie>> recommendationList = movieService.generateRecommendations(users, movies);
 
